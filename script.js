bfa835422f97104bed4d994481f1fa61b6301e5a
Misha <michael@skorik.com>
2024-07-31 13:21:57 +0400

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Задача 1
+
+const F = function(C) {
+    return 9 / 5 * C + 32;
+}
+console.log(F(null)); //Введите любое число
+
+//Задача 2
+
+
+let name = "Name";
+let admin = name;
+console.log(admin)
